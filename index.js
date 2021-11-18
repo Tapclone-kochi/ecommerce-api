fb1b4794dc4bc469ba9300446cb84df61545798f
George Jose <jgeorge97@gmail.com>
2021-11-18 17:08:49 +0530

    Added new user API & fixed cors issue

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const mongoose = require('mongoose');
 
 require('dotenv').config()
 
-var cors = require('cors')
-app.use(cors())
-
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
