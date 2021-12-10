2764ae804f865c0133ab492d6ddf72686c527252
George Jose <jgeorge97@gmail.com>
2021-12-10 16:12:55 +0530

    Updated middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
     try {
         const token = req.header("x-auth-token");
-        if (!token) return res.status(401).send({ error: true, msg: "Access denied." });
+        if (!token) return res.status(401).send({ error: true, msg: "Access denied!! Login to your account" });
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
         req.user = decoded;
