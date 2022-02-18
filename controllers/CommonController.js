2857eb465d5654faab376c9d70a7f01467959a87
firoz-git <firoskhansha@gmail.com>
2022-02-18 14:00:40 +0530

    total amount displaying in the dashboard completed

--- controllers/CommonController.js
+++ controllers/CommonController.js
@@ -1,5 +1,5 @@
 const User = require('../models/User');
-const Order = require('../models/Order')
+const Order = require('../models/Order') 
 
 class CommonController {
   getDashboardData = async (req, res) => {
@@ -7,16 +7,18 @@ class CommonController {
       const userCount = await User.countDocuments()
       const placedOrderCount = await Order.countDocuments({ status: 'order_placed' })
       const dispatchedOrderCount = await Order.countDocuments({ status: 'order_dispatched' })
-      const totalAmount = await Order.aggregate([
+      const totalAmount = await Order.aggregate([{ "$match": {$or:[{ "status":"order_dispatched" },{ "status":"order_placed" }]} },
         {
           $group: {
+            _id: null,
             totalAmount: {
               $sum: "$totalAmount"
             },
-            _id: null
+            
           }
         }
       ])
+      console.log(totalAmount);
       res.send({ error: false, userCount: userCount, placedOrderCount: placedOrderCount, dispatchedOrderCount: dispatchedOrderCount, totalAmount: totalAmount[0].totalAmount/100 })
     } catch (error) {
       console.error(error);
