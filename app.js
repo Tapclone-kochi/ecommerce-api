ede897232a09e51b9f7b30090756406e5601c72f
firoz-git <firoskhansha@gmail.com>
2022-05-17 16:44:42 +0530

    serverless deployment successfull

--- app.js
+++ app.js
@@ -2,11 +2,23 @@ const express = require('express');
 const app = express();
 var cors = require('cors')
 var morgan = require('morgan')
+const mongoose = require('mongoose');
+require('dotenv').config()
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
 app.use(cors())
 app.use(morgan('combined'));
+
+mongoose
+  .connect(process.env.MONGO_DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    autoIndex: true,
+    keepAlive: true,
+  })
+  .then(() => console.log('Database Connected Successfully'))
+  .catch((error) => console.log('ERROR : Database Connection Failed', error));
 
 const routes = require('./routes/api')
 
--- index.js
+++ index.js
@@ -8,15 +8,6 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
-mongoose
-  .connect(process.env.MONGO_DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    autoIndex: true,
-    keepAlive: true,
-  })
-  .then(() => console.log('Database Connected Successfully'))
-  .catch((error) => console.log('ERROR : Database Connection Failed', error));
 
 server.listen(port, () => {
   console.log(`Server is running on port:${port}`);
